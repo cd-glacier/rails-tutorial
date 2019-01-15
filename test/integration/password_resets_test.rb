bb3e89f60ddebd4abda904f409c4df181a64ed97
hyoga-yoshida <hyoga-yoshida@cookpad.com>
2019-01-15 12:18:41 +0900

    Finish chapter 12

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -70,6 +70,9 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
     assert_redirected_to user
+
+    # exercise.4
+    assert_nil user.reload.reset_digest
   end
 
   test "expired token" do
