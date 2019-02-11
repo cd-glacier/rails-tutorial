b03c7759a92c175342c79a8db95f3524c875d168
g-hyoga <hyoga0216@gmail.com>
2019-02-11 18:43:21 +0900

    Finish 13.4.1

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -6,20 +6,28 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "micropost interface" do
+  test 'micropost interface' do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # 無効な送信
     assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: "" } }
+      post microposts_path, params: { micropost: { content: '' } }
     end
     assert_select 'div#error_explanation'
     # 有効な送信
-    content = "This micropost really ties the room together"
+    content = 'This micropost really ties the room together'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: {
+        micropost: {
+          content: content,
+          picture: picture
+        }
+      }
     end
+    assert @user.microposts.paginate(page: 1).first.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
